10dd711a50d539406e649176eff9885f7d5762af
batotarayenn <rbenabdallah402@gmail.com>
2022-03-16 23:05:52 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Profile from "./profile/Profile";
+
+
+const App=()=> {
+  // function HandleName({fullName}) {
+  //   alert(`Hello from ${fullName}`)
+// }
+  return (
+    <div>
+     < Profile fullName="Hamza Zaazaa" 
+     Bio="Hello World" 
+     profession="Video Editor"
+      />
+     
+    </div>
+  );
+}
+
+export default App;
